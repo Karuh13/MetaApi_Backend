55a6d4e6a8f7d077874f853d3e0e096e0c2cdf7e
JaviAzagra <javiazagra12@gmail.com>
2022-10-29 11:28:28 +0200

    FEAT: added library.model.js and trim file.model.js

--- src/api/files/file.model.js
+++ src/api/files/file.model.js
@@ -5,12 +5,12 @@ const Schema = mongoose.Schema;
 const fileSchema = new Schema(
     {
 
-        name: {type: String, required: true},
+        name: {type: String, required: true, trim: true},
         libraries: [{type: mongoose.Schema.Types.ObjectId, required: true, ref: "libraries"}],
-        rute: {type: String, required: true},
-        description: {type: String},
+        rute: {type: String, required: true, trim: true},
+        description: {type: String, trim: true},
         type: {type: String, enum: ["file", "folder"], required: true},
-        img: {type: String, defaul:"https://pbs.twimg.com/profile_images/1473729155506319360/TjcpeV6k_400x400.jpg"}
+        img: {type: String, defaul:"https://pbs.twimg.com/profile_images/1473729155506319360/TjcpeV6k_400x400.jpg", trim: true}
     },
     {
         timestamps: true
---
+++ src/api/libraries/library.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const librarySchema = new Schema (
+    {
+        name: {type: String, enum: ["bcrypt", "cloudinary", "cors", "dotenv", "express", "express-validator", "jsonwebtoken", "mongoose", "mongoose-unique-validator", "multer", "multer-storage-cloudinary", "nodemon"], required:true},
+        description: {type: String, required: true, trim: true},
+        version:{type: String, trim: true, default: "not specified"},
+        img: {type: String, defaul:"https://pbs.twimg.com/profile_images/1473729155506319360/TjcpeV6k_400x400.jpg", trim:true},
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Library = mongoose.model("libraries", librarySchema);
+
+module.exports = Library;

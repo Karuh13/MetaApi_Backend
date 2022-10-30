6664d6a80f616878925ffad4defea6dc42cb8c5b
Karuh13 <alvaro.c.s.13@gmail.com>
2022-10-30 22:59:52 +0100

    FIX: field rute to route in file.model.js

--- src/api/files/file.model.js
+++ src/api/files/file.model.js
@@ -7,7 +7,7 @@ const fileSchema = new Schema(
 
         name: {type: String, required: true, trim: true},
         libraries: [{type: mongoose.Schema.Types.ObjectId, required: true, ref: "libraries"}],
-        rute: {type: String, required: true, trim: true},
+        route: {type: String, required: true, trim: true},
         description: {type: String, trim: true},
         type: {type: String, enum: ["file", "folder"], required: true},
         img: {type: String, default:"https://pbs.twimg.com/profile_images/1473729155506319360/TjcpeV6k_400x400.jpg", trim: true}
